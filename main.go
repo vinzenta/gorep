ddfe31a91bf68df5b6ac2182f3dbe596a8385221
Geoffrey White <geoffpiercewhite@gmail.com>
2025-11-24 18:51:08 -0500

    adding tests + major changes

--- main.go
+++ main.go
@@ -13,8 +13,47 @@ import (
 	"fortio.org/terminal/ansipixels/tcolor"
 )
 
+type config struct {
+	trim       bool
+	file       string
+	outputPath string
+	re         *regexp.Regexp
+	args       []string
+}
+
+func newConfig(re *regexp.Regexp, trim bool, file string, outputPath string, args []string) *config {
+	return &config{
+		trim:       trim,
+		file:       file,
+		outputPath: outputPath,
+		re:         re,
+		args:       args,
+	}
+}
+
+func Configure() *config {
+	if len(os.Args) < 2 {
+		panic("gorep needs a pattern to match")
+	}
+	noTrim := flag.Bool("no-trim", false,
+		"disable trimming leading indentation in each line when printed")
+	fileFlag := flag.String("f", "", "take input from a file or directory")
+	outputFile := flag.String("o", "", "save the matches to a file")
+	flag.Parse()
+	args := flag.Args()
+	var re *regexp.Regexp
+	var err error
+	_ = flag.CommandLine.Parse(args[1:])
+	re, err = regexp.Compile(args[0])
+	if err != nil {
+		panic(fmt.Sprintf("%v", err))
+	}
+	return newConfig(re, !*noTrim, *fileFlag, *outputFile, args)
+}
+
 func main() {
-	Main()
+	c := Configure()
+	c.Main()
 }
 
 var (
@@ -24,64 +63,46 @@ var (
 	BLUE  = tcolor.Blue.Foreground()
 )
 
-func Main() {
-	if len(os.Args) < 2 {
-		log.Fatal(("gorep requires a pattern to search"))
-	}
+func (c *config) Main() int {
 	lines := [][2]int{{}}
-	noTrim := flag.Bool("no-trim", false,
-		"disable trimming leading indentation in each line when printed")
-	fileFlag := flag.String("f", "", "take input from a file or directory")
-	outputFile := flag.String("o", "", "save the matches to a file")
-	flag.Parse()
-	args := flag.Args()
-	var re *regexp.Regexp
-	var err error
-	err = flag.CommandLine.Parse(args[1:])
-	if err != nil {
-		log.Fatal("bad input")
-	}
 	var opf *os.File
-	if *outputFile != "" {
-		_, err = os.ReadFile(*outputFile)
+	if c.outputPath != "" {
+		_, err := os.ReadFile(c.outputPath)
 		if err == nil {
-			log.Fatal("output file already exists")
+			log.Print("output file already exists")
+			return 1
 		}
-		opf, err = os.Create(*outputFile)
+		opf, err = os.Create(c.outputPath)
 		if err != nil {
-			log.Fatal("output file couldn't be created")
+			log.Print("output file couldn't be created")
+			return 1
 		}
 		defer opf.Close()
 	}
 
-	re, err = regexp.Compile(args[0])
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
 	var str string
-	if len(args) > 1 {
-		str = strings.Join(args[1:], " ")
+	if len(c.args) > 1 {
+		str = strings.Join(c.args[1:], " ")
 	}
 	switch {
-	case *fileFlag != "":
-		info, err := os.Stat(*fileFlag)
+	case c.file != "":
+		info, err := os.Stat(c.file)
 		if err != nil {
 			fmt.Println("can't open given file or directory")
-			return
+			return 1
 		}
 		if info.IsDir() {
-			files := recursiveFileSearch(*fileFlag)
-			matchAllChildren(re, *noTrim, files, opf)
-			return
+			files := recursiveFileSearch(c.file)
+			c.matchAllChildren(files, opf)
+			return 0
 		}
-		content, err := os.ReadFile(*fileFlag)
+		content, err := os.ReadFile(c.file)
 		str = string(content)
 		if err != nil {
 			fmt.Println("can't open given file")
-			return
+			return 1
 		}
-	case len(args) < 2:
+	case len(c.args) < 2:
 		scanner := bufio.NewScanner(os.Stdin)
 		var builder strings.Builder
 		index := 0
@@ -93,28 +114,29 @@ func Main() {
 			lines = append(lines, [2]int{builder.Len()})
 			if err != nil {
 				fmt.Println("invalid input")
-				return
+				return 1
 			}
 		}
 		str = builder.String()
 	}
-	match(re, *noTrim, str, "", opf)
+	c.match(str, "", opf)
+	return 0
 }
 
-func matchAllChildren(re *regexp.Regexp, noTrim bool, children [][2]string, outputFile *os.File) {
+func (c *config) matchAllChildren(children [][2]string, outputFile *os.File) {
 	for _, file := range children {
 		preString := fmt.Sprintf("%s%s: \n", BLUE, file[0])
-		match(re, noTrim, file[1], preString, outputFile)
+		c.match(file[1], preString, outputFile)
 	}
 }
 
-func match(re *regexp.Regexp, noTrim bool, str string, preString string, output *os.File) {
+func (c *config) match(str string, preString string, output *os.File) {
 	i := 0
 	emptyCount := 0
 	printString := ""
 	for line := range strings.Lines(str) {
-		matches := re.FindAllString(line, -1)
-		indices := re.FindAllStringIndex(line, -1)
+		matches := c.re.FindAllString(line, -1)
+		indices := c.re.FindAllStringIndex(line, -1)
 		if len(matches) == 0 {
 			emptyCount++
 			i++
@@ -126,7 +148,7 @@ func match(re *regexp.Regexp, noTrim bool, str string, preString string, output 
 		lengthMatches := len(matches)
 		for j, m := range matches {
 			pre := line[curI:indices[j][0]]
-			if !noTrim {
+			if c.trim {
 				pre = strings.TrimLeft(pre, "\t")
 			}
 			matchBuilder.WriteString(fmt.Sprintf("%s%s%s%s", pre, GREEN, m, WHITE))
@@ -135,13 +157,13 @@ func match(re *regexp.Regexp, noTrim bool, str string, preString string, output 
 				continue
 			}
 			post := line[indices[j][1]:]
-			if !noTrim {
+			if c.trim {
 				post = strings.TrimRight(post, "\t\n")
 			}
 			matchBuilder.WriteString(post)
 		}
 		matchString := matchBuilder.String()
-		if !noTrim {
+		if c.trim {
 			matchString = strings.Trim(matchString, " ")
 		}
 		printString = fmt.Sprintf("%s%s\n", printString, matchString)
@@ -157,10 +179,6 @@ func match(re *regexp.Regexp, noTrim bool, str string, preString string, output 
 		for _, toRemove := range []string{GREEN, RED, BLUE, WHITE} {
 			forOutputFile = strings.ReplaceAll(forOutputFile, toRemove, "")
 		}
-		// forOutputFile := strings.ReplaceAll(printString, GREEN, "")
-		// forOutputFile = strings.ReplaceAll(forOutputFile, RED, "")
-		// forOutputFile = strings.ReplaceAll(forOutputFile, BLUE, "")
-		// forOutputFile = strings.ReplaceAll(forOutputFile, WHITE, "")
 		_, err := output.WriteString(forOutputFile)
 		if err != nil {
 			fmt.Println("couldn't write output")
@@ -174,8 +192,13 @@ func recursiveFileSearch(path string) [][2]string {
 	if err != nil {
 		log.Fatal("can't open pwd")
 	}
-	if path != "." && path != "./" && path != ".\\" && pwd[len(pwd)-len(path):] != path {
+
+	if path != "." && path != "./" && path != ".\\" && len(pwd)-len(path) > 0 && pwd[len(pwd)-len(path):] != path {
 		pwd += "/" + path
+	}
+
+	if path != "." && (path[:2] == "C:" || path[0] == '/') {
+		pwd = path
 	}
 	entries, err := os.ReadDir(pwd)
 	if err != nil {
@@ -196,7 +219,6 @@ func recursiveFileSearch(path string) [][2]string {
 		}
 		contents, err := os.ReadFile(e.Name())
 		if err != nil || !utf8.Valid(contents) {
-			fmt.Println(err)
 			continue
 		}
 		files = append(files, [2]string{e.Name(), string(contents)})
---
+++ main_test.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"os"
+	"regexp"
+	"testing"
+)
+
+func TestInvalidArgs(t *testing.T) {
+	tests := []struct {
+		name        string
+		args        []string
+		shouldPanic bool
+	}{
+		{
+			name:        "NoArguments",
+			args:        []string{"gorep"},
+			shouldPanic: true,
+		},
+		{
+			name:        "InvalidRegex",
+			args:        []string{"gorep", "[invalid", "valid", "test.txt"},
+			shouldPanic: true,
+		},
+		{
+			name:        "InvalidFlag",
+			args:        []string{"gorep", "-invalid", "test", "replacement"},
+			shouldPanic: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			os.Args = tt.args
+			if tt.shouldPanic {
+				defer func() {
+					if r := recover(); r == nil {
+						t.Errorf("Expected panic for test case: %s", tt.name)
+					}
+				}()
+			}
+			Configure() // Call the main function directly
+		})
+	}
+}
+
+func TestValidArgs(t *testing.T) {
+	pwd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("can't get pwd")
+	}
+	os.Args = []string{"gorep", "test", "this is a test"}
+	c := newConfig(regexp.MustCompile("const[a-z]{3}"), true, ".", t.TempDir()+"/result", nil)
+	if c.Main() != 0 {
+		t.Fail()
+	}
+	t.Chdir(pwd)
+	c.file = "main_test.go"
+	c.outputPath = ""
+	if c.Main() != 0 {
+		t.Fail()
+	}
+}
+
+func TestRecursiveFileSearch(t *testing.T) {
+	pwd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("%v", pwd)
+	}
+	tempDir := t.TempDir()
+	files := recursiveFileSearch(tempDir)
+	if len(files) != 0 {
+		t.Errorf("Expected no files, got %d", len(files))
+	}
+
+	tempDir = t.TempDir()
+	filePath := tempDir + "/file.txt"
+	err = os.WriteFile(filePath, []byte("content"), 0o644)
+	t.Chdir(pwd)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	files = recursiveFileSearch(tempDir)
+	if len(files) != 1 || files[0][0] != "file.txt" {
+		t.Errorf("Expected one file named 'file.txt', got %v", files)
+	}
+
+	tempDir = t.TempDir()
+	t.Chdir(pwd)
+	nestedDir := tempDir + "/nested"
+	os.Mkdir(nestedDir, 0o755)
+	filePath = nestedDir + "/file.txt"
+	os.WriteFile(filePath, []byte("content"), 0o644)
+	files = recursiveFileSearch(tempDir)
+	if len(files) != 1 || files[0][0] != "file.txt" {
+		t.Errorf("Expected one file named 'file.txt' in nested directory, got %v", files)
+	}
+}
